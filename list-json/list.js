5346df61752cb662e17e8c343bf586d7739fe1e6
ekatalmu <ekatalmu@gmail.com>
2022-01-21 12:06:52 +0300

    Fixed mobile grid, 2.6 - request goes to random API

--- list-json/list.js
+++ list-json/list.js
@@ -3,16 +3,33 @@
 let textJson = document.querySelector(".add-json");
 
 function submitForm(event){
-    event.preventDefault();
-    let text = JSON.stringify($('form').serializeArray());
-    textJson.textContent = text;
-    // $.ajax({
-    //     type: "GET",
-    //     url: "server.php",
-    //     success: function(){
-    //       alert("t");
-    //     },
-    //   });
-  }
 
+    event.preventDefault(); //чтобы при отправке формы не перезагружалась страница
+    let text = JSON.stringify($('form').serializeArray()); //формирование JSON-структуры из данных формы
+    textJson.textContent = text; //выводим данные в пустой параграф под формой
+
+//   2.6 Форма должна отправлять асинхронный GET-запрос к абстрактному
+//   обработчику на том же сервере (в той же папке). В случае, если данные
+//   дошли, надо получать ответ. Можно выводить его через alert.
+
+//   Запрос отправляется к рандомному API вместо абстрактного обработчика на сервере
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://some-random-api.ml/facts/dog", true);
+    xhr.onload = function (e) {
+        if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+            alert("Ответ с сервера: " + xhr.responseText);
+        } else {
+            console.error(xhr.statusText);
+        }
+    }
+    };
+    xhr.onerror = function (e) {
+    console.error(xhr.statusText);
+    };
+    xhr.send(null);
+}
+
+  
 
